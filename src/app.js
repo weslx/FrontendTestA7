97d4c5351bc6388ff20210e0541f4fdfe555ef79
Wesley <wesleyaleixo0950@gmail.com>
2024-10-31 21:02:48 -0300

    Feat: Path solution

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
+import router from "./routes/index.js";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/", indexRouter);
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
+app.use("/", router);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
